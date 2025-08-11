61fceaf61aacdbcced120575c9944fbe276a94cc
Hiruni Dilmika <117562801+HiruDilmi@users.noreply.github.com>
2025-08-11 12:25:56 +0530

    updated Checkout backend

--- Backend/routes/customer/CheckoutRoutes.js
+++ Backend/routes/customer/CheckoutRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const checkoutController = require('../../controllers/customer/CheckoutController');
+const { checkCompanyCode } = require('../../middleware/customer/CustomerValidation');
+const { optionalAuth } = require('../../middleware/customer/CustomerAuth');
 
-
-router.post('/checkout-details', checkoutController.submitCheckout);
+router.post('/checkout-details', checkCompanyCode, optionalAuth, checkoutController.submitCheckout);
 
 module.exports = router;
